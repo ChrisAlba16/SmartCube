5896de2587ebb1ae98ea7808d8d5c3162bfed6a8
Christopher Alba Arcos <al057202@uacam.mx>
2022-12-16 14:51:43 -0600

    Gracias por tanto, perdon por tan poco

--- backend/first-gear-app/src/lib.rs
+++ backend/first-gear-app/src/lib.rs
@@ -1,64 +1,47 @@
 #![no_std]
-use gstd::{prelude::*, ActorId};
-use scale_info::TypeInfo;
 
-#[derive(Clone, Debug, Encode, Decode, TypeInfo, Default, PartialEq, Eq)]
-pub struct Contract {
-    pub id: ActorId,
-    pub autor: ActorId,
-    pub program: String,
-    pub description: Description,
-    pub rate: i32,
-    pub audited: bool,
-    pub label: String,
-}
+use gstd::{debug, msg, prelude::*};
 
-impl Contract {
-    pub fn new(
-        id: ActorId,
-        autor: ActorId,
-        program: String,
-        description: Description,
-        rate: i32,
-        audited: bool,
-        label: String,
-    ) -> Self {
-        Self {
-            id,
-            autor,
-            program,
-            description,
-            rate,
-            audited,
-            label,
+static mut MESSAGE_LOG: Vec<String> = vec![];
+
+#[no_mangle]
+extern "C" fn handle() {
+    let new_msg = String::from_utf8(msg::load_bytes().expect("Invalid message"))
+        .expect("Unable to create string");
+
+    if new_msg == "PING" {
+        msg::reply_bytes("PONG", 0).expect("Unable to reply");
+    }
+
+    unsafe {
+        MESSAGE_LOG.push(new_msg);
+
+        debug!("{:?} total message(s) stored: ", MESSAGE_LOG.len());
+
+        for log in &MESSAGE_LOG {
+            debug!(log);
         }
     }
-    pub fn set_id(&mut self, id: ActorId) {
-        self.id = id;
-    }
-    pub fn set_rate(&mut self, rate: i32) {
-        self.rate = rate;
-    }
-    pub fn set_audited(&mut self, audited: bool) {
-        self.audited = audited;
-    }
-    pub fn set_auditor_description(&mut self, auditor_description: String) {
-        self.description
-            .set_auditor_description(auditor_description);
-    }
-    pub fn set_label(&mut self, label: String) {
-        self.label = label;
-    }
 }
 
-#[derive(Clone, Debug, Encode, Decode, TypeInfo, Default, PartialEq, Eq)]
-pub struct Description {
-    pub owner_description: String,
-    pub auditor_description: String,
-}
+#[cfg(test)]
+mod tests {
+    extern crate std;
 
-impl Description {
-    pub fn set_auditor_description(&mut self, auditor_description: String) {
-        self.auditor_description = auditor_description;
+    use gtest::{Log, Program, System};
+
+    #[test]
+    fn it_works() {
+        let system = System::new();
+        system.init_logger();
+
+        let program = Program::current(&system);
+
+        let res = program.send_bytes(42, "INIT");
+        assert!(res.log().is_empty());
+
+        let res = program.send_bytes(42, "PING");
+        let log = Log::builder().source(1).dest(42).payload_bytes("PONG");
+        assert!(res.contains(&log));
     }
 }
